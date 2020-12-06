ed93f403fa00e9fe31df406e0b123d6d78ef8760
scottkc97 <scott97kc@gmail.com>
2020-12-05 21:39:26 -0600

    Update advent2020_04.R

--- advent2020_04.R
+++ advent2020_04.R
@@ -57,3 +57,35 @@ passport %>%
 #' Count the number of valid passports - those that have 
 #' all required fields and valid values. Continue to treat 
 #' cid as optional. In your batch file, how many passports are valid?
+
+#' byr (Birth Year) - four digits; at least 1920 and at most 2002.
+#' iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+#' eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+#' hgt (Height) - a number followed by either cm or in:
+#'   If cm, the number must be at least 150 and at most 193.
+#'   If in, the number must be at least 59 and at most 76.
+#' hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+#' ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+#' pid (Passport ID) - a nine-digit number, including leading zeroes.
+#' cid (Country ID) - ignored, missing or not.
+
+
+# apply rules above to respective columns, prefix v
+validated_passport <- passport %>% 
+  # 1 for valid // 0 for invalid 
+  mutate(
+    vbyr = ifelse(between(byr, 1920, 2002) & nchar(byr) == 4, 1, 0),
+    viyr = ifelse(between(iyr, 2010, 2020) & nchar(iyr) == 4, 1, 0),
+    veyr = ifelse(between(eyr, 2020, 2030) & nchar(eyr) == 4, 1, 0),
+    vhgt = ifelse((hgt_unit == "cm" & between(hgt, 150, 193)) |
+                  (hgt_unit == "in" & between(hgt, 59, 76)) , 1, 0),
+    vhcl = ifelse(str_detect(hcl, "^#[0-9a-f]{6}$"), 1, 0),
+    vecl = ifelse(str_detect(ecl, "^(amb|blu|brn|gry|grn|hzl|oth)$"), 1, 0),
+    vpid = ifelse(str_detect(pid, "^[0-9]{9}$"), 1, 0)
+  )
+
+validated_passport %>% 
+  # If the last 7 columns are all valid (sum = 7) ...
+  mutate(valid = ifelse(rowSums(.[10:16]) == 7, TRUE, FALSE)) %>% 
+  # Return distro of valid, not valid, NA
+  count(valid)
